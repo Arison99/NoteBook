d837bd8647d3d1dd6053bf2f40c31533868239c0
Arison99 <harrisondaviinci@gmail.com>
2025-11-29 12:57:36 +0300

    Create __init__.py

---
+++ backend/src/notebook/__init__.py
@@ -0,0 +1,5 @@
+"""NoteBook - GraphQL Learning Application with Distributed Replication"""
+
+__version__ = "1.0.0"
+__author__ = "Arison99"
+__description__ = "GraphQL learning project with CouchDB distributed replication"
